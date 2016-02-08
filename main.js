d6e200c2ccedc51ad6fdeda3c9057df2d7c027f0
Tom Lovett <tomlovett6@gmail.com>
2016-02-08 16:41:43 -0700

    slowly building mode-switching

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var DataOpoly = angular.module('Data-opoly', [])
 
-DataOpoly.controller('primary', ['$scope', 'utility', 'preloads', function($scope, utility, preloads) {
+DataOpoly.controller('primary', ['$scope', 'process', 'preloads', 'route', 'load', function($scope, process, preloads, route, load) {
 
 	$scope.tiles     = preloads.tiles
 	$scope.topRow    = preloads.topRow
@@ -8,14 +8,13 @@ DataOpoly.controller('primary', ['$scope', 'utility', 'preloads', function($scop
 	$scope.rightCol  = preloads.rightCol
 	$scope.bottomRow = preloads.bottomRow
 
-
-	$scope.mode = '' // game_taps, first_taps, loc_by_turn, tapped, pred, success
+	$scope.mode = 'gameTaps'
 
 	$scope.changeMode = function(mode) {
 		$scope.mode = mode
-
+		route.change(mode, $scope)
+		$scope.bottomText = preloads.text[mode]
 	}
-
 
 	$scope.fire = function(id) {
 		console.log('fire: ', id)
@@ -26,14 +25,9 @@ DataOpoly.controller('primary', ['$scope', 'utility', 'preloads', function($scop
 		}
 	}
 
-	var gameTaps = utility.gameTaps
-	gameTaps = _.mapObject(gameTaps, function(obj) {
-		return obj['perc_of_whole']
-		// return obj['std_dev'] / obj['avg']
-	})
-
-	var colors = utility.processRawData(gameTaps)
-	// colors = utility.processRawData(utility.tappedTen)
+	var displayData = function(id, mode) {
+		return '?'
+	}
 
 	var assignColors = function(colorSet) {
 		$scope.tiles[10.5].setColor(colorSet[10.5])
@@ -41,11 +35,22 @@ DataOpoly.controller('primary', ['$scope', 'utility', 'preloads', function($scop
 			$scope.tiles[i].setColor(colorSet[i])
 		}
 	}
+/* pre-loading display because I like it */
+	var gameTaps = preloads.gameTaps
+	gameTaps = _.mapObject(gameTaps, function(obj) {
+		return obj['perc_of_whole']
+		// return obj['std_dev'] / obj['avg']
+	})
+
+	var colors = process.processRawData(gameTaps)
+	// colors = process.processRawData(preloads.tappedTen)
+
+
 	assignColors(colors)
 
 }])
 
-DataOpoly.factory('utility', function() {
+DataOpoly.factory('process', function() {
 
 	var processRawData = function(data) {
 		var min = _.min(_.values(data))
@@ -64,7 +69,7 @@ DataOpoly.factory('utility', function() {
 		return output
 	}
 
-	var numberToColor = function(val) {
+	var numberToColor = function(val) {  // this section needs cleaning up
 		var floor   = 0
 		var ceiling = 0
 		var diff    = 0
@@ -101,57 +106,8 @@ DataOpoly.factory('utility', function() {
 		return colorVal.toString()
 	}
 
-	var tappedTen = {"10.5": 0.6904, "0": 0.584, "2": 0.6177, "3": 0.6657, "4": 0.7047, "5": 0.7413, "6": 0.7796, "1": 0.5767, "8": 0.7928, "9": 0.7807, "10": 0.7603, "11": 0.7469, "12": 0.7425, "13": 0.7103, "14": 0.7082, "15": 0.7346, "16": 0.7357, "17": 0.7359, "18": 0.7471, "19": 0.7481, "20": 0.7471, "21": 0.7387, "22": 0.7343, "23": 0.744, "24": 0.7404, "25": 0.7288, "26": 0.7272, "27": 0.7269, "28": 0.7151, "29": 0.7111, "30": 0.6904, "31": 0.7102, "32": 0.6939, "33": 0.6753, "34": 0.6666, "35": 0.6523, "36": 0.6215, "37": 0.6008, "38": 0.5984, "39": 0.5891, "7": 0.8042}
-
-	var gameTaps = {
-		"0": {"median": 6.0, "avg": 5.7477, "std_dev": 2.25744, "perc_of_whole": 0.02113}, 
-		"1": {"median": 6.0, "avg": 5.7227, "std_dev": 2.22234, "perc_of_whole": 0.02104}, 
-		"2": {"median": 6.0, "avg": 5.8581, "std_dev": 2.27661, "perc_of_whole": 0.02154}, 
-		"3": {"median": 6.0, "avg": 6.0535, "std_dev": 2.32625, "perc_of_whole": 0.02226}, 
-		"4": {"median": 6.0, "avg": 6.2062, "std_dev": 2.33094, "perc_of_whole": 0.02282}, 
-		"5": {"median": 6.0, "avg": 6.4099, "std_dev": 2.36349, "perc_of_whole": 0.02357}, 
-		"6": {"median": 6.0, "avg": 6.6249, "std_dev": 2.38281, "perc_of_whole": 0.02436}, 
-		"7": {"median": 7.0, "avg": 6.9216, "std_dev": 2.49067, "perc_of_whole": 0.02545}, 
-		"8": {"median": 7.0, "avg": 6.861, "std_dev": 2.43731, "perc_of_whole": 0.02523}, 
-		"9": {"median": 7.0, "avg": 6.7967, "std_dev": 2.44044, "perc_of_whole": 0.02499}, 
-		"10": {"median": 7.0, "avg": 6.6389, "std_dev": 2.39305, "perc_of_whole": 0.02441}, 
-		"11": {"median": 6.0, "avg": 6.5302, "std_dev": 2.38073, "perc_of_whole": 0.02401}, 
-		"12": {"median": 6.0, "avg": 6.4199, "std_dev": 2.3747, "perc_of_whole": 0.02361}, 
-		"13": {"median": 6.0, "avg": 6.247, "std_dev": 2.34013, "perc_of_whole": 0.02297}, 
-		"14": {"median": 6.0, "avg": 6.2573, "std_dev": 2.33069, "perc_of_whole": 0.02301}, 
-		"15": {"median": 6.0, "avg": 6.5016, "std_dev": 2.40994, "perc_of_whole": 0.02391}, 
-		"16": {"median": 6.0, "avg": 6.5792, "std_dev": 2.42469, "perc_of_whole": 0.02419}, 
-		"17": {"median": 7.0, "avg": 6.7444, "std_dev": 2.43205, "perc_of_whole": 0.0248}, 
-		"18": {"median": 7.0, "avg": 6.8719, "std_dev": 2.44366, "perc_of_whole": 0.02527}, 
-		"19": {"median": 7.0, "avg": 7.0587, "std_dev": 2.46918, "perc_of_whole": 0.02595}, 
-		"20": {"median": 7.0, "avg": 7.091, "std_dev": 2.49061, "perc_of_whole": 0.02607}, 
-		"21": {"median": 7.0, "avg": 7.1033, "std_dev": 2.48428, "perc_of_whole": 0.02612}, 
-		"22": {"median": 7.0, "avg": 7.1796, "std_dev": 2.52613, "perc_of_whole": 0.0264}, 
-		"23": {"median": 7.0, "avg": 7.3229, "std_dev": 2.51357, "perc_of_whole": 0.02693}, 
-		"24": {"median": 7.0, "avg": 7.2455, "std_dev": 2.46224, "perc_of_whole": 0.02664}, 
-		"25": {"median": 7.0, "avg": 7.2255, "std_dev": 2.48082, "perc_of_whole": 0.02657}, 
-		"26": {"median": 7.0, "avg": 7.268, "std_dev": 2.50427, "perc_of_whole": 0.02672}, 
-		"27": {"median": 7.0, "avg": 7.1565, "std_dev": 2.48153, "perc_of_whole": 0.02631}, 
-		"28": {"median": 7.0, "avg": 6.9933, "std_dev": 2.44913, "perc_of_whole": 0.02571}, 
-		"29": {"median": 7.0, "avg": 6.9256, "std_dev": 2.43291, "perc_of_whole": 0.02547}, 
-		"30": {"median": 7.0, "avg": 7.0829, "std_dev": 2.56629, "perc_of_whole": 0.02604}, 
-		"31": {"median": 7.0, "avg": 7.1555, "std_dev": 2.48659, "perc_of_whole": 0.02631}, 
-		"32": {"median": 7.0, "avg": 6.9255, "std_dev": 2.42057, "perc_of_whole": 0.02546}, 
-		"33": {"median": 7.0, "avg": 6.7572, "std_dev": 2.43944, "perc_of_whole": 0.02485}, 
-		"34": {"median": 6.0, "avg": 6.5776, "std_dev": 2.3693, "perc_of_whole": 0.02419}, 
-		"35": {"median": 6.0, "avg": 6.403, "std_dev": 2.34448, "perc_of_whole": 0.02354}, 
-		"36": {"median": 6.0, "avg": 6.1065, "std_dev": 2.36921, "perc_of_whole": 0.02245}, 
-		"37": {"median": 6.0, "avg": 5.7786, "std_dev": 2.25849, "perc_of_whole": 0.02125}, 
-		"38": {"median": 6.0, "avg": 5.7637, "std_dev": 2.27149, "perc_of_whole": 0.02119}, 
-		"39": {"median": 6.0, "avg": 5.7688, "std_dev": 2.24529, "perc_of_whole": 0.02121},
-		"10.5": {"median": 7.0, "avg": 7.0829, "std_dev": 2.56629, "perc_of_whole": 0.02604}
-	}
-	var totalTaps = 2719648
-
 	return {
-		processRawData : processRawData,
-		tappedTen : tappedTen,
-		gameTaps : gameTaps
+		processRawData : processRawData
 	}
 })
 
@@ -219,6 +175,15 @@ DataOpoly.factory('preloads', function() {
 	}
 
 
+	var text = {
+		gameTaps : 'GAME TAPS!!',
+		firstTaps : 'FIRST TAPS!!',
+		locByTurn : 'LOCATION BY TURN!!',
+		tapped : 'TAPPED!!',
+		preds : 'PREDECESSORS!!',
+		succs : 'SUCCESSORS!!'
+	}
+
 	var topRow = _.map([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], function(index) {
 		return tiles[index]	})
 	var leftCol = _.map([19, 18, 17, 16, 15, 14, 13, 12, 11], function(index) {
@@ -228,17 +193,82 @@ DataOpoly.factory('preloads', function() {
 	var bottomRow = _.map([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], function(index) {
 		return tiles[index]	})
 
+	var tappedTen = {"10.5": 0.6904, "0": 0.584, "2": 0.6177, "3": 0.6657, "4": 0.7047, "5": 0.7413, "6": 0.7796, "1": 0.5767, "8": 0.7928, "9": 0.7807, "10": 0.7603, "11": 0.7469, "12": 0.7425, "13": 0.7103, "14": 0.7082, "15": 0.7346, "16": 0.7357, "17": 0.7359, "18": 0.7471, "19": 0.7481, "20": 0.7471, "21": 0.7387, "22": 0.7343, "23": 0.744, "24": 0.7404, "25": 0.7288, "26": 0.7272, "27": 0.7269, "28": 0.7151, "29": 0.7111, "30": 0.6904, "31": 0.7102, "32": 0.6939, "33": 0.6753, "34": 0.6666, "35": 0.6523, "36": 0.6215, "37": 0.6008, "38": 0.5984, "39": 0.5891, "7": 0.8042}
+
+	var gameTaps = {
+		"0":  { "std_dev": 2.25744, "perc_of_whole": 0.02113 }, 
+		"1":  { "std_dev": 2.22234, "perc_of_whole": 0.02104 }, 
+		"2":  { "std_dev": 2.27661, "perc_of_whole": 0.02154 }, 
+		"3":  { "std_dev": 2.32625, "perc_of_whole": 0.02226 }, 
+		"4":  { "std_dev": 2.33094, "perc_of_whole": 0.02282 }, 
+		"5":  { "std_dev": 2.36349, "perc_of_whole": 0.02357 }, 
+		"6":  { "std_dev": 2.38281, "perc_of_whole": 0.02436 }, 
+		"7":  { "std_dev": 2.49067, "perc_of_whole": 0.02545 }, 
+		"8":  { "std_dev": 2.43731, "perc_of_whole": 0.02523 }, 
+		"9":  { "std_dev": 2.44044, "perc_of_whole": 0.02499 }, 
+		"10": { "std_dev": 2.39305, "perc_of_whole": 0.02441 }, 
+		"11": { "std_dev": 2.38073, "perc_of_whole": 0.02401 }, 
+		"12": { "std_dev": 2.3747,  "perc_of_whole": 0.02361 }, 
+		"13": { "std_dev": 2.34013, "perc_of_whole": 0.02297 }, 
+		"14": { "std_dev": 2.33069, "perc_of_whole": 0.02301 }, 
+		"15": { "std_dev": 2.40994, "perc_of_whole": 0.02391 }, 
+		"16": { "std_dev": 2.42469, "perc_of_whole": 0.02419 }, 
+		"17": { "std_dev": 2.43205, "perc_of_whole": 0.0248  }, 
+		"18": { "std_dev": 2.44366, "perc_of_whole": 0.02527 }, 
+		"19": { "std_dev": 2.46918, "perc_of_whole": 0.02595 }, 
+		"20": { "std_dev": 2.49061, "perc_of_whole": 0.02607 }, 
+		"21": { "std_dev": 2.48428, "perc_of_whole": 0.02612 }, 
+		"22": { "std_dev": 2.52613, "perc_of_whole": 0.0264  }, 
+		"23": { "std_dev": 2.51357, "perc_of_whole": 0.02693 }, 
+		"24": { "std_dev": 2.46224, "perc_of_whole": 0.02664 }, 
+		"25": { "std_dev": 2.48082, "perc_of_whole": 0.02657 }, 
+		"26": { "std_dev": 2.50427, "perc_of_whole": 0.02672 }, 
+		"27": { "std_dev": 2.48153, "perc_of_whole": 0.02631 }, 
+		"28": { "std_dev": 2.44913, "perc_of_whole": 0.02571 }, 
+		"29": { "std_dev": 2.43291, "perc_of_whole": 0.02547 }, 
+		"30": { "std_dev": 2.56629, "perc_of_whole": 0.02604 }, 
+		"31": { "std_dev": 2.48659, "perc_of_whole": 0.02631 }, 
+		"32": { "std_dev": 2.42057, "perc_of_whole": 0.02546 }, 
+		"33": { "std_dev": 2.43944, "perc_of_whole": 0.02485 }, 
+		"34": { "std_dev": 2.3693,  "perc_of_whole": 0.02419 }, 
+		"35": { "std_dev": 2.34448, "perc_of_whole": 0.02354 }, 
+		"36": { "std_dev": 2.36921, "perc_of_whole": 0.02245 }, 
+		"37": { "std_dev": 2.25849, "perc_of_whole": 0.02125 }, 
+		"38": { "std_dev": 2.27149, "perc_of_whole": 0.02119 }, 
+		"39": { "std_dev": 2.24529, "perc_of_whole": 0.02121 },
+		"10.5": {"std_dev": 2.56629, "perc_of_whole": 0.02604}
+	}
+
+	var totalTaps = 2719648
+
 	return {
 		tiles     : tiles,
 		topRow    : topRow,
 		leftCol   : leftCol,
 		rightCol  : rightCol,
-		bottomRow : bottomRow
+		bottomRow : bottomRow,
+		tappedTen : tappedTen,
+		gameTaps  : gameTaps,
+		text 	  : text
 	}
 })
 
-DataOpoly.factory('route', function() {
-	var load = function(path) {
+// 'route' and 'load' very similar, could combine into one?
 
+DataOpoly.factory('route', function() {
+	var change = function(mode, scope) {
+		if (mode !== 'locByTurn' && mode !== 'tapped') {
+			scope.turnSelector = false
+		} else {
+			scope.turnSelector = true
+		}
 	}
+
+	return {
+		change : change
+	}
+})
+
+DataOpoly.factory('load', function() {
+	return 'data'
 })
